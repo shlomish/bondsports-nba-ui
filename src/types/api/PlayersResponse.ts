98acbea1804761d5ee7322fdbc63392205307c71
Shlomish <shtauber.shlomi@gmail.com>
2023-12-12 11:41:43 +0200

    📝 Add base typing

---
+++ src/types/api/PlayersResponse.ts
@@ -0,0 +1,5 @@
+import { Player } from "../Player";
+
+export interface PlayersResponse {
+  data: Player[];
+}
---
+++ src/types/index.ts
@@ -0,0 +1,3 @@
+export * from "./Player";
+export * from "./Team";
+export * from "./api";
